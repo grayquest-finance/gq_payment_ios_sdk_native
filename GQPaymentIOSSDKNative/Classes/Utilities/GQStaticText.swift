99261a6eba373077cb24b0c48409a37e26866075
Valentine Miranda <valentine.miranda@grayquest.com>
2024-03-01 13:09:34 +0530

    Added Utilities

---
+++ GQPaymentIOSSDKNative/Classes/Utilities/GQStaticText.swift
@@ -0,0 +1,17 @@
+//
+//  GQStaticText.swift
+//  GQPaymentIOSSDKNative
+//
+//  Created by valentine on 01/03/24.
+//
+
+import Foundation
+
+enum GQStaticText {
+    
+    static let mobileNumberPlaceholder = "Enter mobile number"
+    
+    
+    
+    
+}
